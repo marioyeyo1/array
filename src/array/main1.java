87016caefd92ed07a3220a78cf613d098c708c51
Mario <Mariomuoz@campusfp.es>
2021-01-25 10:06:47 -1200

    2 snapshot Main1.java array double

--- src/array/main1.java
+++ src/array/main1.java
@@ -7,7 +7,17 @@ public class main1 {
             String [] nombre = {"luis","miguel","carlos"};
             //recorrer array
             for(int i=0; i<nombre.length; i++){
-            System.out.println(nombre[i]);}
+            System.out.println(nombre[i]);
+            
+            }
+            //declaracion de array tipo duble
+            double [] estatura = {1.72,1.75,1.65};
+            //recorrer el array
+            for(int i=0; i<estatura.length; i++){
+            System.out.println(estatura[i]);
+            }
+            
+            
     }
     
 }
