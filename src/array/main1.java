27e8fd57f0e432cbaccef50e7adab6ed07d3ddb1
Mario <Mariomuoz@campusfp.es>
2021-01-25 10:01:27 -1200

    1 snapshot Main.java

---
+++ src/array/main1.java
@@ -0,0 +1,13 @@
+
+package array;
+
+public class main1 {
+
+    public static void main(String[] args) {
+            String [] nombre = {"luis","miguel","carlos"};
+            //recorrer array
+            for(int i=0; i<nombre.length; i++){
+            System.out.println(nombre[i]);}
+    }
+    
+}
